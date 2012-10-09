300fd84100aff020d0d48b4173138d786c307a67
Rachael LaMielle <rachael.lamielle@gmail.com>
2012-10-08 20:15:38 -0700

    Created tool
    Autocomplete auto-populates and on select desired entry is added to
    body.

---
+++ managers.js
@@ -0,0 +1,5 @@
+var managers = [
+{id: 'Bob_Smith', name: 'Bob Smith (TK)', location: 'CA', group: 'C Group', description: 'jkfhgekjrt'},
+{id: 'Bob_1', name: 'Bob the first', location: 'MI', group: 'B Group', description: 'lololololol'},
+{id: 'Kitty_Cat', name: 'Daphne', location: 'CO', group: 'A Group', description: 'lelelelelelele'}
+];
